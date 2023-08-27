6df5b70c107344432d6f42accdd4a0800ffd0ed9
M.N. Jeral Sandeeptha <jeral.sandeeptha1@gmail.com>
2023-08-27 13:26:51 +0530

    Implement AWS bucket image uploading functionality

--- routes/blog.js
+++ routes/blog.js
@@ -1,16 +1,40 @@
 const express = require('express');
 const BlogSchema = require('../models/Blog');
+const AWS = require('aws-sdk');
+const multerS3 = require('multer-s3');
+const multer = require('multer');
 
 const router = express.Router();
 
-router.post('/', async (req, res) => {
+AWS.config.update({
+    accessKeyId: process.env.ACCESS_KEY,
+    secretAccessKey: process.env.ACCESS_SECRET,
+    region: process.env.REGION,
+});
+
+const s3 = new AWS.S3();
+
+// Configure multer and multer-s3
+const upload = multer({
+    storage: multerS3({
+      s3: s3,
+      bucket: process.env.BUCKET,
+      key: function (req, file, cb) {
+        cb(null, Date.now().toString() + '-' + file.originalname);
+      },
+    }),
+});
+
+router.post('/', upload.single('image'), async (req, res) => {
     try {
+
+        console.log(req.file);
 
         const newBlog = new BlogSchema({
             title: req.body.title,
             author: req.body.author,
             description: req.body.description,
-            image: req.body.image
+            image: req.file.location,
         });
 
         const savedBlog = await newBlog.save();
@@ -61,10 +85,16 @@ router.get('/:id', async (req, res) => {
     }
 });
 
-router.put('/:id', async (req, res) => {
+router.put('/:id', upload.single('image'),async (req, res) => {
     try {
+        console.log(req.file);
 
-        const updatedBlog = await BlogSchema.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true });
+        const updatedBlog = await BlogSchema.findByIdAndUpdate(req.params.id, { $set: {
+            title: req.body.title,
+            author: req.body.author,
+            description: req.body.description,
+            image: req.file.location
+        } }, { new: true });
 
         res.status(201).json({
             message: "Update blog query successful",
