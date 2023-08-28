8c235b40594f31cfef47178ee9259e425d456e0d
M.N. Jeral Sandeeptha <101165998+JeralSandeeptha@users.noreply.github.com>
2023-08-28 13:45:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 const blogRoutes = require('./routes/blog');
+const postRoutes = require('./routes/post');
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/users', blogRoutes);
+app.use('/posts', postRoutes);
 
 const PORT = process.env.PORT || 8000;
 
